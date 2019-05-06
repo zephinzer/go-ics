ca3da9e6d61d35009c7c5b74fc7a957ae0aaa37b
zephinzer <dev@joeir.net>
2019-05-07 01:22:21 +0800

    still wip

--- calendar.go
+++ calendar.go
@@ -1,13 +1,53 @@
 package ics
 
+import "fmt"
+
 const ICalHeader = "BEGIN:VCALENDAR"
-const ICalVersion = "VERSION:2.0"
 const ICalFooter = "END:VCALENDAR"
 
+const DefaultCalendarProductID = "zephinzer/go-ics"
+const DefaultCalendarCalscale = "GREGORIAN"
+const DefaultCalendarMethod = CalendarMethodPublish
+const DefaultCalendarVersion = 2.0
+
+const CalendarMethodPublish = "PUBLISH"
+const CalendarMethodRequest = "REQUEST"
+
 type Calendar struct {
-	Events []Event `json:"events"`
+	Version   float64 `json:"version"`    // https://icalendar.org/iCalendar-RFC-5545/3-7-4-version.html
+	Calscale  string  `json:"calscale"`   // https://icalendar.org/iCalendar-RFC-5545/3-7-1-calendar-scale.html
+	Method    string  `json:"method"`     // https://icalendar.org/iCalendar-RFC-5545/3-7-2-method.html
+	ProductID string  `json:"product_id"` // https://icalendar.org/iCalendar-RFC-5545/3-7-3-product-identifier.html
+	Events    []Event `json:"events"`     // https://icalendar.org/iCalendar-RFC-5545/3-6-1-event-component.html
 }
 
 func (calendar *Calendar) Serialize() string {
-	return ""
+	c := ICalHeader + "\n"
+
+	// Version generation
+	if calendar.Version == 0.0 {
+		calendar.Version = DefaultCalendarVersion
+	}
+	c += "VERSION:" + fmt.Sprintf("%f", calendar.Version) + "\n"
+
+	// CalScale generation
+	if len(calendar.Calscale) == 0 {
+		calendar.Calscale = DefaultCalendarCalscale
+	}
+	c += "CALSCALE:" + calendar.Calscale + "\n"
+
+	// Method generation
+	if len(calendar.Method) == 0 {
+		calendar.Method = DefaultCalendarMethod
+	}
+	c += "METHOD:" + calendar.Method + "\n"
+
+	// ProductID generation
+	if len(calendar.ProductID) == 0 {
+		calendar.ProductID = DefaultCalendarProductID
+	}
+	c += "PRODID:" + calendar.ProductID + "\n"
+
+	c += ICalFooter + "\n"
+	return c
 }
--- event.go
+++ event.go
@@ -1,27 +1,62 @@
 package ics
 
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
 const ICalEventHeader = "BEGIN:VEVENT"
 const ICalEventFooter = "END:VEVENT"
 
+// Event (https://icalendar.org/iCalendar-RFC-5545/3-6-1-event-component.html)
 type Event struct {
-	UID         string    `json:"uid"`
-	ProductID   string    `json:"product_id"`
-	CalScale    string    `json:"calscale"`
-	Method      string    `json:"method"`
-	Title       string    `json:"title"`
-	Summary     string    `json:"summary"`
-	Description string    `json:"description"`
-	URL         string    `json:"url"`
-	Categories  []string  `json:"categories"`
-	Status      string    `json:"status"`
-	Organizer   Person    `json:"organizer"`
-	Location    Location  `json:"geo"`
-	Timestamp   Timestamp `json:"timestamp"`
-	Sequence    int       `json:"sequence"`
+	UID            string    `json:"uid"`
+	Title          string    `json:"title"`
+	Summary        string    `json:"summary"`     // https://icalendar.org/iCalendar-RFC-5545/3-8-1-12-summary.html
+	Description    string    `json:"description"` // https://icalendar.org/iCalendar-RFC-5545/3-8-1-5-description.html
+	URL            string    `json:"url"`
+	Categories     []string  `json:"categories"`
+	Status         string    `json:"status"`
+	Organizer      Person    `json:"organizer"`
+	Location       Location  `json:"geo"`
+	Timestamp      Timestamp `json:"timestamp"`
+	Sequence       int       `json:"sequence"`
+	RecurrenceRule string    `json:"recurrence_rule"`
 }
 
 func (event *Event) Serialize() string {
-	return ICalEventHeader + `
-		
-	`
+	e := ICalEventHeader + "\n"
+
+	// UID generation
+	if len(event.UID) == 0 {
+		event.UID = uuid.New().String()
+	}
+	e += "UID:" + event.UID + "\n"
+
+	// Title
+	if len(event.Title) > 0 {
+		e += "TITLE:" + event.Title
+	}
+
+	// Summary
+	if len(event.Summary) > 0 {
+		e += "SUMMARY:" + event.Summary
+	}
+
+	// Description
+	if len(event.Description) > 0 {
+		e += "DESCRIPTION:" + event.Description
+	}
+
+	// Summary
+	if len(event.Summary) > 0 {
+		e += "SUMMARY:" + event.Summary
+	}
+
+	// Sequence
+	e += "SEQUENCE:" + fmt.Sprintf("%v", event.Sequence) + "\n"
+
+	e += ICalEventFooter + "\n"
+	return e
 }
